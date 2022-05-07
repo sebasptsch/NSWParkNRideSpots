16b9f185d4490c7c27632bb8a78c6155c0f24171
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-05-07 12:08:27 +0000

    add container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,15 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="columns">
-      <div className="column"></div>
-      <div className="column is-3 my-6">
-        <Component {...pageProps} />
-      </div>
+    <div className="container">
+      <div className="columns">
+        <div className="column"></div>
+        <div className="column is-3 my-6">
+          <Component {...pageProps} />
+        </div>
 
-      <div className="column"></div>
+        <div className="column"></div>
+      </div>
     </div>
   );
 }
